d441a3775200a5f733287132b438ec0c1b2b5447
Eugene Alforov <eugenalforov@gmail.com>
2018-01-22 00:43:06 +0200

    Redirected to client html

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /* GET home page. */
 router.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  response.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
 });
 
 module.exports = router;
