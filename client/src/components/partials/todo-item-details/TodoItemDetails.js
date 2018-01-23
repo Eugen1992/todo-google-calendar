7d051811e68fff75324b3da6443c4d2ec7d8ea69
Eugene Alforov <eugenalforov@gmail.com>
2018-01-23 12:44:01 +0200

    Added expanding

--- client/src/components/partials/todo-item-details/TodoItemDetails.js
+++ client/src/components/partials/todo-item-details/TodoItemDetails.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Loader from '../../ui/loader';
-import Header from '../../partials/header';
 import moment from 'moment';
+import './TodoItemDetails.css';
 
-export default function TodoDetails ({ isLoading, summary, description, createdAt, dueDate, startsAt }) {
-  if (isLoading) return (<Loader />);
-  return (<div>
-    <Header text={summary} />
+export default function TodoDetails ({ description, dueDate, createdAt, startsAt }) {
+  return (<div className="todoDetails-container">
     <div>Description: { description }</div><br />
     <div>Start date: { moment(startsAt).calendar() }</div><br />
     <div>Created at: { moment(createdAt).calendar() }</div>
--- client/src/components/partials/todo-item/TodoItem.js
+++ client/src/components/partials/todo-item/TodoItem.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import './TodoItem.css';
 import { Link } from 'react-router-dom';
+import TodoItemDetails from '../todo-item-details';
 
-export default function TodoItem({ description, id, onRemove, startsAt  }) {
-  const isChecked = new Date(startsAt).getTime() < new Date().getTime();
+export default class TodoItem extends React.Component {
+  constructor(...args) {
+    super(...args);
 
-  return (
-    <div className="container">
-      <div>
-        <input checked={isChecked} type="checkbox" disabled className={isChecked ? 'toggle isChecked' : 'toggle'} />
-        <Link className="description" to={`details/${id}`}>{ description }</Link>
+    this.state = { expanded: false };
+    this.toggle = this.toggle.bind(this);
+  }
+  toggle() {
+    this.setState({
+      expanded: !this.state.expanded,
+    })
+  }
+  render() {
+    const { description, id, onRemove, startsAt  } = this.props;
+    const { expanded } = this.state;
+    const isChecked = new Date(startsAt).getTime() < new Date().getTime();
+
+    return (
+      <div className="container">
+        <div className="row">
+          <input checked={isChecked} type="checkbox" disabled className={isChecked ? 'toggle isChecked' : 'toggle'} />
+          <Link className="description" to={`details/${id}`}>{ description }</Link>
+          <span className="expand" onClick={this.toggle} />
+        </div>
+        <span className="close" onClick={onRemove} />
+        <div className="fullLine">
+        { expanded && <TodoItemDetails {...this.props} /> }
+        </div>
       </div>
-      <span className="close" onClick={onRemove} ></span>
-    </div>
-  )
+    )
+  }
+
 }
